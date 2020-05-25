290fd31276fb52b227d93d4b2af292a03b616f9a
Guybrush Threepwood <benoit.development@gmail.com>
2020-05-25 17:23:46 +0200

    Add webpack config

--- api/src/routes/swagger.js
+++ api/src/routes/swagger.js
@@ -2,10 +2,10 @@ import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 import fs from 'fs'
 import yaml from 'js-yaml'
-import appRoot from 'app-root-path'
+import path from 'path'
 
 const router = express.Router()
-const file = fs.readFileSync(appRoot + '/static/swagger.yml', 'utf8');
+const file = fs.readFileSync(path.join(process.cwd(), 'static/swagger.yml'), 'utf8');
 const swaggerDocument = yaml.safeLoad(file);
 
 router.use('/', function (req, res, next) {
