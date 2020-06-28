1bcf64b1448226777f4ba539398028fe3e94f468
benoit-devlopment <benoit.development@gmail.com>
2020-06-28 12:28:14 +0200

    App simplier version

--- temp/tests/components/HelloComponent.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import HomeComponent from '../../src/components/HomeComponent.vue'
-
-describe('HelloWorld.vue', () => {
-  test('renders props.msg when passed', () => {
-    const wrapper = shallowMount(HomeComponent)
-    // expect(wrapper.text()).toContain('homepage.jpg')
-  })
-})
