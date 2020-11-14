b6615f86c407e034c593deba585536af62dd10ad
benoit-development <benoit.development@gmail.com>
2020-11-14 20:13:03 +0100

    github action bug fix

--- api/test/setup.js
+++ api/test/setup.js
@@ -7,6 +7,7 @@ import AppUser from '../src/models/user.js'
 const { MongoMemoryServer } = MongoMemoryServerPkg
 
 let mongoServer;
+let user;
 
 beforeEach(async () => {
     // set a memory instance of mongodb
@@ -14,7 +15,7 @@ beforeEach(async () => {
     const mongoUri = await mongoServer.getUri()
     await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
 
-    AppUser.collection.insertOne({
+    user = await AppUser.collection.insertOne({
         login: 'benoit',
         name: 'Benoît',
         password: '$2b$10$1o3KY/tO81e0/4nPhG5MVO04p8R0i90r12cNLD2l3qfdpdgPrQG3.',
@@ -32,5 +33,6 @@ beforeEach(async () => {
 
 afterEach(async () => {
     await mongoose.disconnect()
+    await AppUser.findOneAndDelete(user.id)
     await mongoServer.stop()
 });
